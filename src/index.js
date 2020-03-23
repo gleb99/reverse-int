6d32ae29452e48477eced2ba6f2c22f3b5dcd2a9
Gleb <gllukutin@gmail.com>
2020-03-23 22:22:34 +0300

    add: add solution of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
+  if (n < 0){
+    n = -n;
+  } 
+  let string = n.toString()
+  let arr = string.split('');
   
+  arr.reverse();
+  return arr.join('');
 }
